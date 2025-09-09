98a60b5fef2607ed107c3b838417a08a00ece7ed
Chanthana <hemchanthana@gmail.com>
2025-09-10 00:53:19 +0700

    Thana makes a Login Page

---
+++ CorsoLaneWeb/Pages/LoginAndSignUpPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CorsoLaneWeb.Pages
+{
+    public class LoginAndSignUpPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
